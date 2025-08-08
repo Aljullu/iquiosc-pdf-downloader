5d0ead1f1350a3a6a86874b4b90300e1f6b0e9d9
Albert Juhé Lluveras <contact@albertjuhe.com>
2025-08-08 19:29:52 +0200

    Update to new PDF editor

--- download.js
+++ download.js
@@ -1,1 +1,13 @@
-window.wrappedJSObject.PDFViewerApplication.download();
+var script = document.createElement("script");
+script.textContent = `
+  var iQuioscPdfDownloaderScript = document.getElementById("webviewer-1");
+  if (iQuioscPdfDownloaderScript.contentWindow.instance) {
+    iQuioscPdfDownloaderScript.contentWindow.instance.downloadPdf();
+  } else {
+    iQuioscPdfDownloaderScript.addEventListener("load", () => {
+      iQuioscPdfDownloaderScript.contentWindow.instance.downloadPdf();
+    });
+  };
+`;
+document.documentElement.appendChild(script);
+script.remove();
